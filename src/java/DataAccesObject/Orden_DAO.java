01115a44bb19d320f3fd670201c34a9aaab9d656
KODE <KODE@KODE-LAP0052>
2019-04-08 19:10:41 -0500

    Total 'correccion de formulario a registro_medico' 08-04-19

--- src/java/DataAccesObject/Orden_DAO.java
+++ src/java/DataAccesObject/Orden_DAO.java
@@ -8,6 +8,7 @@ import DataTransferObject.Estadistica_DTO;
 import DataTransferObject.Observacion_DTO;
 import DataTransferObject.Orden_DTO;
 import DataTransferObject.Pago_DTO;
+import DataTransferObject.Participacion_DTO;
 import DataTransferObject.Resultado_DTO;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -36,21 +37,18 @@ public class Orden_DAO {
                     + "'" + orden.getEstado() + "',"
                     + "'" + orden.getConvenio() + "',"
                     + "" + orden.getFolio_Unidad() + ")";
-            System.out.println(sql);
             try (PreparedStatement pstm = con.prepareStatement(sql);) {
                 pstm.executeUpdate();
             }
             sql = "select id_Orden from orden where id_unidad=" + orden.getUnidad().getId_Unidad() + " and id_paciente=" + orden.getPaciente().getId_Paciente() + ""
                     + " and id_medico=" + orden.getMedico().getId_Medico() + " and id_Persona=" + orden.getEmpleado().getId_Persona() + ""
                     + " and Fecha_Orden='" + orden.getFecha() + "'  and Hora_Orden='" + orden.getHora() + "' and Precio_Total='" + Util.redondearDecimales(orden.getMontoPagado()) + "' and montoRes='" + Util.redondearDecimales(orden.getMontoRestante()) + "' and convenio='" + orden.getConvenio() + "' and folio_unidad=" + orden.getFolio_Unidad() + "";
-            System.out.println(sql);
             try (PreparedStatement pstm = con.prepareStatement(sql); ResultSet rs = pstm.executeQuery()) {
                 while (rs.next()) {
                     orden.setId_Orden(rs.getInt("id_Orden"));
                 }
             }
             if (orden.getId_Orden() != 0) {
-                System.out.println("ORDEN REGISTRADA");
                 Det_Orden_DAO DET = new Det_Orden_DAO();
                 orden.getDet_Orden().forEach((detor) -> {
                     detor.setId_det_orden(DET.registrarDetor(orden.getId_Orden(), detor));
@@ -62,8 +60,21 @@ public class Orden_DAO {
                         pago.setId_pago(P.registrarPago(pago));
                     });
                 }
+                if (orden.getMedico() != null & orden.getMedico().getParticipacion() != null) {
+                    Float monto = orden.getMontoPagado() + orden.getMontoRestante();
+                    Float montoPart = monto * (orden.getMedico().getParticipacion() / 100);
+                    Participacion_DTO participacion = new Participacion_DTO();
+                    participacion.setConvenio(orden.getConvenio());
+                    participacion.setFecha(orden.getFecha());
+                    participacion.setHora(orden.getHora());
+                    participacion.setId_Medico(orden.getMedico().getId_Medico());
+                    participacion.setId_Orden(orden.getId_Orden());
+                    participacion.setId_Unidad(orden.getUnidad().getId_Unidad());
+                    participacion.setMonto(montoPart);
+                    Participacion_DAO Pa = new Participacion_DAO();
+                    Pa.registrarPago(participacion);
+                }
             }
-
         } catch (SQLException ex) {
             throw new RuntimeException(ex);
         }
@@ -244,6 +255,100 @@ public class Orden_DAO {
             throw new RuntimeException(e);
         }
         return ord;
+    }
+
+    public List<Orden_DTO> getOrdenes() {
+        List<Orden_DTO> ords = new ArrayList<>();
+        Estudio_DAO E = new Estudio_DAO();
+        Unidad_DAO U = new Unidad_DAO();
+        Paciente_DAO P = new Paciente_DAO();
+        Medico_DAO M = new Medico_DAO();
+        Persona_DAO Pr = new Persona_DAO();
+        try (Connection con = Conexion.getCon()) {
+            String sql = "SELECT * FROM orden order by id_Orden asc";
+            try (PreparedStatement pstm = con.prepareStatement(sql); ResultSet rs = pstm.executeQuery();) {
+                while (rs.next()) {
+                    Orden_DTO ord = new Orden_DTO();
+                    ord.setId_Orden(rs.getInt("id_Orden"));
+                    ord.setUnidad(U.getUnidadAll(rs.getInt("id_Unidad")));
+                    //ord.setPaciente(P.getPaciente(rs.getInt("id_Paciente")));
+                    ord.setMedico(M.getMedico(rs.getInt("id_Medico")));
+                    //ord.setEmpleado(Pr.getPersona(rs.getInt("id_Persona")));
+                    ord.setFecha(rs.getString("Fecha_Orden"));
+                    ord.setHora(rs.getString("Hora_Orden"));
+                    ord.setMontoPagado(rs.getFloat("Precio_Total"));
+                    ord.setMontoRestante(rs.getFloat("montoRes"));
+                    ord.setEstado(rs.getString("Estado"));
+                    ord.setConvenio(rs.getString("convenio"));
+                    ord.setFolio_Unidad(rs.getInt("folio_unidad"));
+                    ords.add(ord);
+                }
+            }
+//            for (Orden_DTO ord : ords) {
+//                List<Det_Orden_DTO> dets = new ArrayList<>();
+//                sql = "SELECT * FROM det_orden WHERE  id_Orden=" + ord.getId_Orden() + "";
+//                try (PreparedStatement pstm = con.prepareStatement(sql); ResultSet rs = pstm.executeQuery();) {
+//                    Boolean r = false;
+//                    while (rs.next()) {
+//                        Det_Orden_DTO det = new Det_Orden_DTO();
+//                        det.setId_det_orden(rs.getInt("id_Det_Orden"));
+//                        det.setEstudio(E.getEst_Uni(rs.getInt("id_Est_Uni")));
+//                        det.setDescuento(rs.getFloat("Descuento"));
+//                        det.setFecha_Entrega(rs.getString("Fecha_Entrega"));
+//                        det.setT_Entrega(rs.getString("Tipo_Entrega"));
+//                        det.setSubtotal(rs.getFloat("Subtotal"));
+//                        String sql1 = "SELECT * FROM resultado WHERE  id_Det_Orden=" + det.getId_det_orden() + "";
+//                        List<Configuracion_DTO> confs = new ArrayList<>();
+//                        try (PreparedStatement pstm1 = con.prepareStatement(sql1); ResultSet rs1 = pstm1.executeQuery();) {
+//                            while (rs1.next()) {
+//                                Configuracion_DTO cnf = new Configuracion_DTO();
+//                                Resultado_DTO res = new Resultado_DTO();
+//                                res.setId_resultado(rs1.getInt("id_resultado"));
+//                                if (res.getId_resultado() != 0) {
+//                                    r = true;
+//                                    cnf.setId_Configuración(rs1.getInt("id_Configuracion"));
+//                                    res.setValor_Obtenido(rs1.getString("Valor_Obtenido"));
+//                                    det.getEstudio().setAddRes(true);
+//                                    String sql2 = "SELECT * FROM configuracion WHERE id_Configuracion=" + cnf.getId_Configuración() + "";
+//                                    try (PreparedStatement pstm2 = con.prepareStatement(sql2); ResultSet rs2 = pstm2.executeQuery();) {
+//                                        while (rs2.next()) {
+//                                            cnf.setDescripcion(rs2.getString("Descripcion"));
+//                                            cnf.setSexo(rs2.getString("sexo"));
+//                                            cnf.setValor_min(rs2.getString("Valor_min"));
+//                                            cnf.setValor_MAX(rs2.getString("Valor_MAX"));
+//                                            cnf.setUniddes(rs2.getString("Unidades"));
+//                                        }
+//
+//                                    }
+//                                    cnf.setRes(res);
+//                                    confs.add(cnf);
+//                                }
+//                            }
+//                        }
+//                        det.getEstudio().getCnfs().forEach((Configuracion_DTO confE) -> {
+//                            confs.stream().filter((confR) -> (confE.getId_Configuración() == confR.getId_Configuración())).forEachOrdered((confR) -> {
+//                                det.getEstudio().getCnfs().set(det.getEstudio().getCnfs().indexOf(confE), confR);
+//                            });
+//                        });
+//                        String sql3 = "SELECT * FROM observacion WHERE  id_Det_Orden=" + det.getId_det_orden() + "";
+//                        try (PreparedStatement pstm3 = con.prepareStatement(sql3); ResultSet rs3 = pstm3.executeQuery();) {
+//                            while (rs3.next()) {
+//                                Observacion_DTO Obs = new Observacion_DTO();
+//                                Obs.setId_Observacion(rs3.getInt("id_Observacion"));
+//                                Obs.setObservacion(rs3.getString("observacion"));
+//                                det.getEstudio().setObservacion(Obs);
+//                            }
+//                        }
+//                        dets.add(det);
+//                    }
+//                    ord.setDet_Orden(dets);
+//
+//                }
+//            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return ords;
     }
 
     public List<Orden_DTO> getOrdenes(int id_Unidad) {
@@ -1140,12 +1245,6 @@ public class Orden_DAO {
 
     public static void main(String[] args) {
         Orden_DAO O = new Orden_DAO();
-        for (Orden_DTO dto : O.getReporteGeneralOrdenes(1, "2018-07-31", "2019-03-17")) {
-            String strEstudios = "";
-            for (Det_Orden_DTO deto : dto.getDet_Orden()) {
-                strEstudios = strEstudios + deto.getEstudio().getClave_Estudio().trim() + ",";
-            }
-            System.out.println(strEstudios);
-        }
+
     }
 }
