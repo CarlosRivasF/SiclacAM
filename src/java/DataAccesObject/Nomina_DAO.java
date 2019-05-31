07910914b37cabbc2027e12b10c4b82b262edc52
Carlos Rivas <Carlos Rivas@DESKTOP-69M6FRD>
2019-05-31 16:25:18 -0500

    PRODUCCIÓN [1..1] 'Aumentar 70 parámetros de Configuración en Registro de Estudios'

--- src/java/DataAccesObject/Nomina_DAO.java
+++ src/java/DataAccesObject/Nomina_DAO.java
@@ -14,9 +14,8 @@ import java.sql.SQLException;
 public class Nomina_DAO {
     
     public void RegistrarNomina(Nomina_DTO Nom, int id_empleado) {        
-        String sql = "INSERT INTO Nomina VALUES(null," + Nom.getPor().getId_Persona() + "," + id_empleado + ","
-                + "'" + Util.redondearDecimales(Nom.getMonto()) + "','" + Nom.getFecha() + "')";
-        
+        String sql = "INSERT INTO nomina VALUES(null," + Nom.getPor().getId_Persona() + "," + id_empleado + ","
+                + "'" + Util.redondearDecimales(Nom.getMonto()) + "','" + Nom.getFecha() + "')";        
         try (Connection con = Conexion.getCon(); PreparedStatement pstm = con.prepareStatement(sql);) {
             pstm.executeUpdate();
         } catch (SQLException ex) {
